0c2fc3d248d6528ce331f6dd9acc0801652d0c30
Omar Farache <omarfarache91@gmail.com>
2024-07-10 18:04:48 +0300

    fix photo

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://preview.pixlr.com/images/800wm/1201/1/1201100331.jpg",
+    img: "https://miro.medium.com/v2/resize:fit:590/1*hc-Z7YgVY_N9UHoYxTF3Sg.jpeg",
     title: "WINTER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
   },
   {
     id: 2,
-    img: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71Pvj7AeW8S._AC_UY500_.jpg",
+    img: "https://www.freevector.com/uploads/vector/preview/22915/autumn_collection.jpg",
     title: "AUTUMN COLLECTION",
     desc: "NEW ARRIVALS.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "https://timberlandpas-chere.fr/wp-content/uploads/2019/12/Knit-Tops.png",
+    img: "https://img.freepik.com/free-photo/fashionable-clothing-collection-hanging-modern-elegant-closet-generated-by-artificial-intelligence_188544-113504.jpg?t=st=1720623803~exp=1720627403~hmac=e619f8ae80e001d0ec26195d12c4e189739723a8ccc0643cf78349ac452155f6&w=740",
     title: "BIG COLLECTION WAITING YOU",
     desc: "NEW ARRIVALS.",
     bg: "fbf0f4",
@@ -27,19 +27,19 @@ export const categories = [
     id: 1,
     img: "https://www.suitsexpert.com/wp-content/uploads/white-collar-business-casual-attire-for-men.jpg",
     title: "CASUAL STYLE!",
-    cat: "casual"
+    cat: "casual",
   },
   {
     id: 2,
     img: "https://i5.walmartimages.com/asr/e41715b2-8ba1-42b0-81b5-194ba3aaf16f.f5db425da6e76f49cf3bbbdd31e67599.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff",
     title: "SPORT LOVER",
-    cat: "sport"
+    cat: "sport",
   },
   {
     id: 3,
     img: "https://images.unsplash.com/photo-1539533018447-63fcce2678e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
     title: "JACKETS",
-    cat: "jackets"
+    cat: "jackets",
   },
 ];
 
@@ -76,4 +76,4 @@ export const popularProducts = [
     id: 8,
     img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
   },
-]
+];
